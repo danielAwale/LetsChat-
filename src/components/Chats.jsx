c8691f5a951f7e1fb25eda356a874ecc707a8f59
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 15:55:31 -0800

    contain some styles for the chats component

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -7,6 +7,7 @@ const Chats = () => {
         <img src="https://scontent.fyvr1-1.fna.fbcdn.net/v/t31.18172-8/13048100_1210519918989003_4755961741411765901_o.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_ohc=lfKnyqOZo7wAX9F1tmS&_nc_ht=scontent.fyvr1-1.fna&oh=00_AfDMyr1imte1Gva5eFaB7f6y1ALBcfKEAXQFVMD2O_NpJQ&oe=63C08533" alt="" />
         <div className="userChatInfo">
           <span>Rahul</span>
+          <p>Hello</p>
         </div>
         </div>
     </div>
