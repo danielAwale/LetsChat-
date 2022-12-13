ddddadddf29edf17e81e2c38e412c55f82578337
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 15:21:00 -0800

    contain the code inside the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=''>Navbar</div>
+    <div className='navbar'>
+      <span className="logo">Letz Chat!</span>
+      <div className="user">
+        <img src="" alt="" />
+        <span>Daniel</span>
+        <button>logout</button>
+      </div>
+    </div>
   )
 }
 
