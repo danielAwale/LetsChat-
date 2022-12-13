9d0cb16e0c372f69126971ec4937c69951777186
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 11:48:58 -0800

    start work on the register page. Form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='formContainer'>
+      <div className='formWrapper'>
+        <form>
+          <input type="text" />
+          <input type="email" />
+          <input type="password" />
+          <input type="file" />
+        </form>
+      </div>
+    </div>
   )
 }
 
