3b74e8a8cf077e42a9dd26183b880ddeacdf11be
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 15:45:27 -0800

    finish the styling for search component input! Will tackle the userchat classname now

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,10 +4,10 @@ const Search = () => {
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type="text" />
+        <input type="text" placeholder='find a user' />
       </div>
       <div className="userChat">
-        <img src="" alt="" />
+        <img src="https://scontent.fyvr1-1.fna.fbcdn.net/v/t31.18172-8/13048100_1210519918989003_4755961741411765901_o.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_ohc=lfKnyqOZo7wAX9F1tmS&_nc_ht=scontent.fyvr1-1.fna&oh=00_AfDMyr1imte1Gva5eFaB7f6y1ALBcfKEAXQFVMD2O_NpJQ&oe=63C08533" alt="" />
         <div className="userChatInfo">
           <span>Rahul</span>
         </div>
