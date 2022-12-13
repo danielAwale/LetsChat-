7617fb5c8cf5bdcc99fe654fcbf64b172d47d2f0
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 11:50:15 -0800

    contain some placeholders

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,9 +5,9 @@ const Register = () => {
     <div className='formContainer'>
       <div className='formWrapper'>
         <form>
-          <input type="text" />
-          <input type="email" />
-          <input type="password" />
+          <input type="text" placeholder='Display Name' />
+          <input type="email" placeholder='Email' />
+          <input type="password" placeholder='Password' />
           <input type="file" />
         </form>
       </div>
