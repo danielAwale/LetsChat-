0c9871ec2f80fa9908a737d121ec167a7039ada3
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 11:51:45 -0800

    contain a couple span and a button

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,11 +4,14 @@ const Register = () => {
   return (
     <div className='formContainer'>
       <div className='formWrapper'>
+        <span className="logo">Letz Chat!</span>
+        <span className="title">Register</span>
         <form>
           <input type="text" placeholder='Display Name' />
           <input type="email" placeholder='Email' />
           <input type="password" placeholder='Password' />
           <input type="file" />
+          <button>Sign Up</button>
         </form>
       </div>
     </div>
