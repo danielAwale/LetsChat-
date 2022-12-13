8dff497a1a5cf287afd10f481a189b2e043c964d
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 15:34:14 -0800

    start work on the search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className="searchForm">
+        <input type="text" />
+      </div>
+      <div className="userChat">
+        <img src="" alt="" />
+        <div className="userChatInfo">
+          <span>Rahul</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
