8e8169c66c5a1b4a905fe15156e82283fd500e85
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 15:07:54 -0800

    start adding styles to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
+import Chat from '../components/Chat'
 
 const Home = () => {
   return (
     <div className='home'>
-      <div className="container"></div>
+      <div className="container">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   )
 }
