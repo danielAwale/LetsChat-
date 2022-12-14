4747046424fb51cd697d8efad74adce302fe210a
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 18:38:58 -0800

    finish styling the message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message'>
+    <div className='message owner'>
       <div className="messageInfo">
         <img src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/2/1-portrait-of-joni-mitchell-jack-robinson.jpg" alt="" />
         <span>Just now</span>
       </div>
       <div className="messageContent">
         <p>Hello</p>
-        <img src=" " alt="" />
+        <img src="https://scontent.fyvr1-1.fna.fbcdn.net/v/t39.30808-6/245474621_6605342672840007_417350582132081964_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_ohc=BQ-_Vxg_xhwAX9twGQ4&_nc_ht=scontent.fyvr1-1.fna&oh=00_AfAo17uhIKjC6p8uHJeNy2_2K7JLn3gjFgZDGMsAZ3fAyg&oe=639E601A" alt="" />
       </div>
     </div>
   )
