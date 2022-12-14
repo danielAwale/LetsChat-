65f4e273bf1f9fc16e192d398d0e20bae66f998b
danielAwale <danielmadhukarawale@gmail.com>
2022-12-14 10:25:04 -0800

    contain some trouble shooting

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,13 +10,14 @@ const Register = () => {
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
-    const displayFile = e.target[3].files[0];
+    const file = e.target[3].files[0];
 
     createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in 
       const user = userCredential.user;
       // ...
+      console.log(user)
     })
     .catch((error) => {
       const errorCode = error.code;
