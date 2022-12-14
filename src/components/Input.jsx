ae80ac637c1cc86a06850855de22065a1ad9fd54
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 18:03:47 -0800

    contain styling for the input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -6,13 +6,14 @@ const Input = () => {
   return (
     <div className='input'>
       <input type="text" placeholder='Type something...' />
-      <div className="send"></div>
+      <div className="send">
       <img src={Attach} alt="" />
       <input type="file" style={{display:"none"}} id="file" />
       <label htmlFor="file">
         <img src={Img} alt="" />
       </label>
       <button>Send</button>
+      </div>
     </div>
   )
 }
