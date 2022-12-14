47980ac4971a654d3bf8dc58879396ab14282ed1
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 16:05:16 -0800

    start styling the chat component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Cam from '../img/cam.png'
+import Add from '../img/add.png'
+import More from '../img/more.png'
 
 const Chat = () => {
   return (
@@ -6,9 +9,9 @@ const Chat = () => {
       <div className="chatInfo">
         <span>Jane</span>
         <div className="chatIcons">
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
         </div>
       </div>
     </div>
