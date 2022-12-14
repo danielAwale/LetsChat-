3bbbdbbd8013fc02698faed4314c82122b17c783
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 18:14:39 -0800

    contain the necessary code in the message component; start on the styling

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message'>Message</div>
+    <div className='message'>
+      <div className="messageInfo">
+        <img src="https://render.fineartamerica.com/images/images-profile-flow/400/images/artworkimages/mediumlarge/2/1-portrait-of-joni-mitchell-jack-robinson.jpg" alt="" />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src=" " alt="" />
+      </div>
+    </div>
   )
 }
 
