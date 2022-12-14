df88da3e2e88911811aecfc76da13474a8706ab5
danielAwale <danielmadhukarawale@gmail.com>
2022-12-13 16:13:45 -0800

    message called in the messages component and messages called in the chat component

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Message from '../components/Message'
 
 const Messages = () => {
   return (
-    <div>Messages</div>
+    <div className='messages'>
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
   )
 }
 
