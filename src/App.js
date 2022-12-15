ec55111f1cad55720268d81a996a9219fec2b0f3
danielAwale <danielmadhukarawale@gmail.com>
2022-12-15 14:41:16 -0800

    contain routes for our pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import './styles.scss'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Register />
-    // <Login />
-    // <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/" index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
