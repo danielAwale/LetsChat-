8b5860e0b4920a5c6bb96ce2578e6043498e6993
danielAwale <danielmadhukarawale@gmail.com>
2022-12-14 23:12:32 -0800

    getting storage set up for firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDS4e0Zuq7wi01SrvoYzX9QGhBOIduWUSk",
   authDomain: "letz-chat-f66c9.firebaseapp.com",
@@ -12,3 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth()
+export const storage = getStorage();
