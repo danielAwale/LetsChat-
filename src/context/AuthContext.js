8e21296c3257eea1466c1b67b3316a0683cfaae9
danielAwale <danielmadhukarawale@gmail.com>
2022-12-17 10:51:06 -0800

    contain a use effect to use the onauthstatechanged function from firebase

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,7 +1,18 @@
-import { createContext, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { createContext, useState, useEffect } from "react";
+import { auth } from "../firebase";
 
 export const AuthContext = createContext()
 
 export const AuthContextProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState()
+
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      console.log(user)
+    })
+  }, [])
+
 }
