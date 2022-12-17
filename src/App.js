d75749736a526b77d72681d7580ee224cc19a916
danielAwale <danielmadhukarawale@gmail.com>
2022-12-17 11:18:24 -0800

    contain protectedRoutes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import './styles.scss'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 
 function App() {
   const { currentUser } = useContext(AuthContext)
-  console.log(currentUser)
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />
+    }
+  }
+
+
+
   return (
     <BrowserRouter BrowserRouter >
       <Routes>
         <Route>
-          <Route path="/" index element={<Home />} />
+          <Route path="/" index element={<ProtectedRoute><Home /></ProtectedRoute>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Route>
