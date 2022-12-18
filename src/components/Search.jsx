498d4b94abb7e711530a68bed3e8e5c52bed0b82
danielAwale <danielmadhukarawale@gmail.com>
2022-12-17 21:48:22 -0800

    start work on the search component functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Search = () => {
+  const [username, setUsername] = useState();
   return (
     <div className='search'>
       <div className="searchForm">
