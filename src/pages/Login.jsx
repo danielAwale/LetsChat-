02d73c7ba29b88d0808f682f3764839e6afb0305
danielAwale <danielmadhukarawale@gmail.com>
2022-12-17 21:25:49 -0800

    work on the Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 import Img from '../img/addAvatar.png'
 
 const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate()
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    try {
+
+    } catch {
+      setErr(true);
+    }
+    
+  }
   return (
     <div className='formContainer'>
       <div className='formWrapper'>
